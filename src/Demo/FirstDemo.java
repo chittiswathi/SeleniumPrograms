e730dd2f4fa774924c7272c80833a5bf8834b2dc
swathi <ch.swathia409@gmail.com>
2023-11-30 22:25:53 +0530

    adding webdrivermanager

--- src/Demo/FirstDemo.java
+++ src/Demo/FirstDemo.java
@@ -2,15 +2,19 @@ package Demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FirstDemo {
 	
 	@Test
 	public void demo1()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\graje\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(chromeOptions);
 		driver.get("https://www.google.com");
 		System.out.println(driver.getTitle());
 		driver.quit();
